3db0430270f234eef476d97d8f39274bf6add1ed
Sagar <30855107+cgsagar@users.noreply.github.com>
2026-07-23 14:56:41 +0530

    Update 5. Implement a function that replicates the behavior of the instanceof operator by manually traversing the prototype chain.js

--- Week3-assignment/5. Implement a function that replicates the behavior of the instanceof operator by manually traversing the prototype chain.js
+++ Week3-assignment/5. Implement a function that replicates the behavior of the instanceof operator by manually traversing the prototype chain.js
@@ -26,3 +26,10 @@ const p = new Person("John");
 console.log(myInstanceOf(p, Person)); // true
 console.log(myInstanceOf(p, Object)); // true
 console.log(myInstanceOf(p, Array));  // false
+
+
+The instanceof operator checks whether a constructor's prototype object exists anywhere in an object's prototype chain. 
+  To replicate it, we start from the object's prototype using Object.getPrototypeOf(), traverse upward through the prototype chain, and 
+  compare each prototype with constructorFn.prototype. If a match is found, return true; if the chain reaches null, return false.
+
+
